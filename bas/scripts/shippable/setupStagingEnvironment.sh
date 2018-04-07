9277ed3b10d0d6f2d3992048b9f16fdfa898a247
limpens <jpmw.limpens@.jp-l.org>
2018-04-07 15:54:05 +0200

    Updated grep for LB name

--- bas/scripts/shippable/setupStagingEnvironment.sh
+++ bas/scripts/shippable/setupStagingEnvironment.sh
@@ -142,7 +142,7 @@ if [[ "$DEBUG" -eq 1 ]]; then
 	ls -l "$WORKINGDIR";
 	terraform output | grep -Eoi 'arn:[^/]+' #grep "$AWS_DEFAULT_REGION" | cut -d'=' -f 2- | sed -e 's/^[ \t]*//';
 fi
-ARN=$(terraform output | grep -Eoi 'arn:[^/]+') # grep "$AWS_DEFAULT_REGION" | cut -d'=' -f 2- | sed -e 's/^[ \t]*//')
+ARN=$(terraform output | grep -Eoi 'arn:.*') # grep "$AWS_DEFAULT_REGION" | cut -d'=' -f 2- | sed -e 's/^[ \t]*//')
 log_debug "LB ARN $ARN"
 if [ "$LOCAL" -eq 0 ]; then
 	shipctl put_resource_state "$LB_RSC_NAME" sourceName "$ARN";
